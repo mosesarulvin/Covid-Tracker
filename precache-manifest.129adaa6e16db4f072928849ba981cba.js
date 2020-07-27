9841182542e86412b4612dde1401f0b2b21a513b
Moses <moses.arulvin@binfac.com>
2020-07-28 01:08:28 +0530

    Updates

---
+++ precache-manifest.129adaa6e16db4f072928849ba981cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e325b1eb25fce0bf2dd35a9370949274",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "685ca43afdbc3b123c77",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e8c64716a20a771f70f3",
+    "url": "/Covid-Tracker/static/css/main.ab8c6ebe.chunk.css"
+  },
+  {
+    "revision": "685ca43afdbc3b123c77",
+    "url": "/Covid-Tracker/static/js/2.1cf2c91f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.1cf2c91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c64716a20a771f70f3",
+    "url": "/Covid-Tracker/static/js/main.dd3ab254.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
